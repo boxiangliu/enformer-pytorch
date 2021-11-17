bc8c2a4aed4c883ab0749e719ed22ef10187593e
Boxiang Liu <jollier.liu@gmail.com>
2021-11-16 20:01:57 -0800

    bugfix

--- model/attention.py
+++ model/attention.py
@@ -177,8 +177,8 @@ def positional_features_central_mask(positions, feature_size, seq_length):
     center_widths = center_widths - 1
     output = (center_widths[None, ...] > positions.abs()[..., None]).float()
 
-    assert output.shape[
-        :-1] == positions.shape & output.shape[-1] == feature_size
+    assert (output.shape[:-1] == positions.shape[:-1] and
+            output.shape[-1] == feature_size)
     return output
 
 
