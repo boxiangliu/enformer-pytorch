a7a483c18b44a893476af4bc5dd1a9bc8778ad47
Boxiang Liu <jollier.liu@gmail.com>
2021-11-16 16:41:39 -0800

    fixed bug

--- model/enformer.py
+++ model/enformer.py
@@ -208,7 +208,7 @@ def exponential_linspace_int(start, end, num, divisible_by=1):
     def _round(x):
         return int(round(x / divisible_by) * divisible_by)
 
-    base = math.exp(math.log(end / start) / (num - 1))
+    base = np.exp(np.log(end / start) / (num - 1))
     return [_round(start * base**i) for i in range(num)]
 
 
